210101f4c7caec883cdb11ec942d99c32318f0bd
exiaohao <exiaohao@gmail.com>
2018-10-24 21:21:46 +0800

    It works

--- src/action.py
+++ src/action.py
@@ -1,15 +1,22 @@
-from .utils.github import (
+from utils.github import (
     GithubAPI,
     GithubAPIMethod,
 )
 
+from config import (
+    Config,
+    logger,
+)
 
-def do_nothing(_):
+
+def do_nothing(req):
+    logger.info('Get nothing todo, action:{}'.format(req.action))
     return {
         'msg': 'nothing to do',
     }
 
 def parse_created(req):
+    logger.info('Get created, req:{}'.format(req))
     github_api = GithubAPI(Config.GITHUB)
     if req.comment.body == '/close':
         return github_api.issue(req.issue.url, 'closed')
--- src/server.py
+++ src/server.py
@@ -2,19 +2,16 @@ import hug
 import json
 import logging
 
-from .utils.github import (
+from utils.github import (
     GithubAPI,
     GithubAPIMethod,
     GithubRequest,
 )
 
-from .action import (
+from action import (
     do_nothing,
     parse_created,
 )
-
-from config import Config
-
 
 logger = logging.getLogger('gunicorn.error')
 
@@ -29,11 +26,13 @@ def hello():
 
 @hug.post('/')
 def capture_action(body):
-    req = GithubRequest(body['payload'])
+    try:
+        req = GithubRequest(body['payload'])
+        act = {
+            'created': parse_created,
+            'unknown': do_nothing,
+        }.get(req.action, do_nothing)
 
-    act = {
-        'created': parse_created,
-        'unknown': do_nothing,
-    }.get(req.action)
-
-    return act(req)
+        return act(req)
+    except TypeError:
+        logger.error(body)
